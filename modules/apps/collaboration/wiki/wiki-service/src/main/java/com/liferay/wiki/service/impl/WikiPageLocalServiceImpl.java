73a2c026017aaf0cebbd5530453e5456e135c263
Roberto Díaz <roberto.diaz@liferay.com>
2018-04-26 16:19:36 +0200

    LPS-80082 All the wiki pages of the same Resource have the same createDate. So for each version the modified date is the actual create date. We should change the method in order to return the latest wWikiPages instead of the latest version of the latest WikiPageResources.

--- modules/apps/collaboration/wiki/wiki-service/src/main/java/com/liferay/wiki/service/impl/WikiPageLocalServiceImpl.java
+++ modules/apps/collaboration/wiki/wiki-service/src/main/java/com/liferay/wiki/service/impl/WikiPageLocalServiceImpl.java
@@ -1517,7 +1517,7 @@ public class WikiPageLocalServiceImpl extends WikiPageLocalServiceBaseImpl {
 
 		cal.add(Calendar.WEEK_OF_YEAR, -1);
 
-		return wikiPageFinder.findByCreateDate(
+		return wikiPageFinder.findByModifiedDate(
 			groupId, nodeId, cal.getTime(), false, start, end);
 	}
 
@@ -1527,7 +1527,7 @@ public class WikiPageLocalServiceImpl extends WikiPageLocalServiceBaseImpl {
 
 		cal.add(Calendar.WEEK_OF_YEAR, -1);
 
-		return wikiPageFinder.countByCreateDate(
+		return wikiPageFinder.countByModifiedDate(
 			groupId, nodeId, cal.getTime(), false);
 	}
 
