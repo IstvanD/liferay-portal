863eedd05606c930ccc0c24f9d39af9d5fdb6d2a
IstvanD <istvan.dezsi@liferay.com>
2019-05-02 12:50:21 +0200

    LPS-92701 Additional check for controlPanel as we may request the GroupFriendlyURL explicitly for the Control Panel layout

--- portal-impl/src/com/liferay/portal/util/PortalImpl.java
+++ portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -8556,7 +8556,7 @@ public class PortalImpl implements Portal {
 						themeDisplay.getSiteGroupId()) &&
 					 (group.getClassPK() != themeDisplay.getUserId()))) {
 
-					if (group.isControlPanel()) {
+					if (group.isControlPanel() || controlPanel) {
 						virtualHostname = themeDisplay.getServerName();
 
 						if (Validator.isNull(virtualHostname) ||
