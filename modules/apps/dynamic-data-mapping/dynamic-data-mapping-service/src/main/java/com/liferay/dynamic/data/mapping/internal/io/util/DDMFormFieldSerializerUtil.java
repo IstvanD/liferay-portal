7ff9047d3bafec1b779d37e8d44f900c07536cbf
Marcell Gyöpös <marcell.gyopos@liferay.com>
2021-04-26 13:53:32 +0200

    LPS-130971 Check string value for null

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/io/util/DDMFormFieldSerializerUtil.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/io/util/DDMFormFieldSerializerUtil.java
@@ -32,6 +32,7 @@ import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
 
 import java.util.List;
 import java.util.Locale;
@@ -310,7 +311,7 @@ public class DDMFormFieldSerializerUtil {
 
 	private static LocalizedValue _trim(LocalizedValue rawLocalizedValue) {
 		if (rawLocalizedValue == null) {
-			return rawLocalizedValue;
+			return null;
 		}
 
 		Map<Locale, String> predefinedValuesMap = rawLocalizedValue.getValues();
@@ -320,10 +321,15 @@ public class DDMFormFieldSerializerUtil {
 		for (Map.Entry<Locale, String> entry : predefinedValuesMap.entrySet()) {
 			String value = entry.getValue();
 
-			value = StringUtil.trim(value);
+			if (Validator.isNotNull(value)) {
+				value = StringUtil.trim(value);
 
-			if (value.length() == 0) {
-				localizedValue.addString(entry.getKey(), StringPool.BLANK);
+				if (value.length() == 0) {
+					localizedValue.addString(entry.getKey(), StringPool.BLANK);
+				}
+				else {
+					localizedValue.addString(entry.getKey(), entry.getValue());
+				}
 			}
 			else {
 				localizedValue.addString(entry.getKey(), entry.getValue());
