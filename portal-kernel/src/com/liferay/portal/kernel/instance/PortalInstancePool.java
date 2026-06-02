11b3f3bdd13e966addba199d7810778edc903ae8
IstvanD <istvan.dezsi@liferay.com>
2026-06-02 16:29:25 +0200

    LPD-82361 Scope getCompanyIds to the current secondary instance

--- portal-kernel/src/com/liferay/portal/kernel/instance/PortalInstancePool.java
+++ portal-kernel/src/com/liferay/portal/kernel/instance/PortalInstancePool.java
@@ -9,9 +9,11 @@ import com.liferay.portal.kernel.dao.jdbc.DataAccess;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
 import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.kernel.util.PropsUtil;
+import com.liferay.portal.kernel.util.PropsValues;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -63,11 +65,12 @@ public class PortalInstancePool {
 
 	public static long[] getCompanyIds() {
 		if (_cacheEnabled) {
-			return ArrayUtil.toLongArray(_portalInstances.keySet());
+			return _filterCompanyIds(
+				ArrayUtil.toLongArray(_portalInstances.keySet()));
 		}
 
 		try {
-			return _getCompanyIdsBySQL();
+			return _filterCompanyIds(_getCompanyIdsBySQL());
 		}
 		catch (SQLException sqlException) {
 			_log.error("Unable to get the company IDs by SQL", sqlException);
@@ -159,6 +162,19 @@ public class PortalInstancePool {
 
 	public static void remove(long companyId) {
 		_portalInstances.remove(companyId);
+	}
+
+	private static long[] _filterCompanyIds(long[] companyIds) {
+		if (!PropsValues.DATABASE_PARTITION_ENABLED ||
+			CompanyThreadLocal.isInitializingPortalInstance() ||
+			CompanyThreadLocal.isUpgradingPortalInstance() ||
+			(CompanyThreadLocal.getNonsystemCompanyId() ==
+				getDefaultCompanyId())) {
+
+			return companyIds;
+		}
+
+		return new long[] {CompanyThreadLocal.getNonsystemCompanyId()};
 	}
 
 	private static long _getCompanyIdBySQL(String webId) throws SQLException {
