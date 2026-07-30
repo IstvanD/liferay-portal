ce116decec200c46075198edc2fe52bdbe9d23a7
IstvanD <istvan.dezsi@liferay.com>
2026-07-30 13:29:19 +0200

    LPD-96721 Store VirtualHost in per partition tables

--- portal-impl/src/com/liferay/portal/db/partition/util/DBPartitionUtil.java
+++ portal-impl/src/com/liferay/portal/db/partition/util/DBPartitionUtil.java
@@ -418,6 +418,15 @@ public class DBPartitionUtil {
 			boolean copyData)
 		throws Exception {
 
+		replaceByTable(
+			connection, companyId, viewName, copyData, StringPool.BLANK);
+	}
+
+	public static void replaceByTable(
+			Connection connection, long companyId, String viewName,
+			boolean copyData, String whereClause)
+		throws Exception {
+
 		if (companyId == _defaultCompanyId) {
 			return;
 		}
@@ -439,7 +448,7 @@ public class DBPartitionUtil {
 						_defaultPartitionName, partitionName, viewName,
 						_getColumnNames(
 							connection, _defaultPartitionName, viewName),
-						StringPool.BLANK));
+						whereClause));
 			}
 		}
 	}
@@ -597,7 +606,8 @@ public class DBPartitionUtil {
 		String targetPartitionName = getPartitionName(toCompanyId);
 
 		try {
-			_copySchema(connection, sourcePartitionName, targetPartitionName);
+			_copySchema(
+				connection, false, sourcePartitionName, targetPartitionName);
 
 			DatabaseMetaData databaseMetaData = connection.getMetaData();
 
@@ -786,8 +796,8 @@ public class DBPartitionUtil {
 	}
 
 	private static void _copySchema(
-			Connection connection, String sourcePartitionName,
-			String targetPartitionName)
+			Connection connection, boolean copyVirtualHostData,
+			String sourcePartitionName, String targetPartitionName)
 		throws SQLException {
 
 		try (PreparedStatement preparedStatement = connection.prepareStatement(
@@ -824,7 +834,10 @@ public class DBPartitionUtil {
 							targetPartitionName, fromTableName, fromTableName));
 
 					if (StringUtil.equalsIgnoreCase(
-							fromTableName, "Configuration_")) {
+							fromTableName, "Configuration_") ||
+						(!copyVirtualHostData &&
+						 StringUtil.equalsIgnoreCase(
+							 fromTableName, "VirtualHost"))) {
 
 						continue;
 					}
@@ -1036,7 +1049,8 @@ public class DBPartitionUtil {
 
 		try {
 			_copySchema(
-				connection, getPartitionName(companyId), exportedPartitionName);
+				connection, true, getPartitionName(companyId),
+				exportedPartitionName);
 
 			DatabaseMetaData databaseMetaData = connection.getMetaData();
 
